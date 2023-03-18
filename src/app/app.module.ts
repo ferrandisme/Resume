08ce682ae9bb2c2cd7c76ba88675a80993444c82
Adrian Ferrandis <adrian92539@gmail.com>
2023-03-18 17:02:44 +0100

    [FEAT] Profile picture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+import { HomeModule } from './home/home.module';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,8 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+    BrowserAnimationsModule,
+    SharedModule,
+    HomeModule  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { DescriptionComponent } from './description/description.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    DescriptionComponent,
   ],
   imports: [
     CommonModule
-  ]
+  ],
 })
 export class HomeModule { }
