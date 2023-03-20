d56a89903b738127345d3d75e6ebee46aa7c75d6
Adrian Ferrandis <adrian92539@gmail.com>
2023-03-20 17:24:47 +0100

    [FEAT] Description and themes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    HomeModule  ],
+    HomeModule ,
+    MatSidenavModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
