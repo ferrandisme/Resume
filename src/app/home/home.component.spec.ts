3a687417466b4f1f042e74f5c918971055fa98e0
Adrian Ferrandis <adrian92539@gmail.com>
2023-03-21 20:50:11 +0100

    [FEAT] Firebase config

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,3 +1,4 @@
+import { DescriptionComponent } from './description/description.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
@@ -8,7 +9,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [DescriptionComponent]
     })
     .compileComponents();
 
